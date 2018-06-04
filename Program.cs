c654c48b3f656462e69a6dad9ad32662984c103f
Myron Oslie <myronoslie@gmail.com>
2018-06-03 21:28:45 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TeleprompterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = ReadFrom("sampleQuotes.txt");
+            Console.WriteLine("Hello World!");
+            foreach (var line in lines)
+            {
+                // Console.WriteLine(line);
+                Console.Write(line);
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    var pause = Task.Delay(200);
+                    // Synchronously waiting on a task is an
+                    // anti-pattern. This will get fixed in later
+                    // steps.
+                    pause.Wait();
+                }
+            }
+        }
+
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // yield return line;
+                    var words = line.Split(' ');
+                    var lineLength = 0;
+                    foreach (var word in words)
+                    {
+                        yield return word + " ";
+                        lineLength += word.Length + 1;
+                        if (lineLength > 70)
+                        {
+                            yield return Environment.NewLine;
+                            lineLength = 0;
+                        }
+                    }
+                    yield return Environment.NewLine;
+                }
+            }
+        }
+
+        private static async Task ShowTeleprompter()
+        {
+            var words = ReadFrom("sampleQuotes.txt");
+            foreach (var word in words)
+            {
+                Console.Write(word);
+                if (!string.IsNullOrWhiteSpace(word))
+                {
+                    await Task.Delay(201);
+                }
+            }
+        }
+
+        private static async Task GetInput()
+        {
+            var delay = 200;
+            Action work = () =>
+            {
+                do
+                {
+                    var key = Console.ReadKey(true);
+                    if (key.KeyChar == '>')
+                    {
+                        delay -= 10; 
+                    }
+                    else if (key.KeyChar == '<')
+                    {
+                        delay += 10;
+                    }
+                } while (true);
+            };
+            await Task.Run(work);
+        }
+    }
+}
